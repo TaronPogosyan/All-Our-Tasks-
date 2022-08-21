eb5abe3081903f85d6f3cb7fae65e97d3033d9c1
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-21 15:07:42 +0300

    Домашняя задания 25

---
+++ Zadacha 25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+// 3⁵ = 3*3*3*3*3, где:
+// 3 — основание степени;
+// 5 — показатель степени.
+
+
+
+Console.WriteLine("введите число A");
+int num1 = Convert.ToInt32(Console.ReadLine()); //Число A
+Console.WriteLine("введите число B");
+int num2 = Convert.ToInt32(Console.ReadLine()); // Число B
+int tretChislo = num1;
+
+for (int i = 1; i < num2; i++)
+{
+    tretChislo *= num1;
+}
+Console.WriteLine("A в натуральную степень B: " + tretChislo);
