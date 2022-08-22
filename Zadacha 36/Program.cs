f50f9fe61329647508527fa3ba18e46765f4129e
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-22 15:40:41 +0300

    Zadacha 36

--- Zadacha 36/Program.cs
+++ Zadacha 36/Program.cs
@@ -2,8 +2,41 @@
 // Найдите сумму элементов, стоящих на нечётных позициях (индексах).
 
 // [3, 7, 23, 12] -> 19
-
 // [-4, -6, 89, 6] -> 0
 
 
-Console.WriteLine("Hello, World!");
+Console.Write("Задайте одномерный массив: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+int[] array = new int[size];
+CreateArrayRndInp(array);
+
+void CreateArrayRndInp(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+PrintArray(array);
+
+for (int a = 0; a < array.Length; a += 2)
+{
+    count = count + array[a];
+}
+
+Console.WriteLine($"Сумму элементов, стоящих на нечётных позициях: {count}.");
