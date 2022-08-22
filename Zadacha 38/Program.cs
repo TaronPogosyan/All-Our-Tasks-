f2aa4a2c2d42f3a85dea2a487962ae872778ef32
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-22 17:11:28 +0300

    Задача 38

--- Zadacha 38/Program.cs
+++ Zadacha 38/Program.cs
@@ -3,6 +3,42 @@
 
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] array = new double[size];
+CreateArrayRndInp(array);
+
+void CreateArrayRndInp(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 10;
+    }
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}; ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+PrintArray(array);
 
 
-Console.WriteLine("Hello, World!");
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int a = 0; a < array.Length; a++)
+{
+    if (array[a] > max) max = array[a];
+
+    if (array[a] < min) min = array[a];
+}
+
+Console.WriteLine($"Разницу между максимальным и минимальным элементами массива: {max - min}.");
