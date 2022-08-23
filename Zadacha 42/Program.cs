58ac750345a274597c2bed6264cd47da611aa3ee
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-23 10:59:40 +0300

    Задача 42

---
+++ Zadacha 42/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+//Вариант 1 (робочий)
+Console.WriteLine("Введите целое положительное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = 0;
+int mult = 1;
+
+while (num > 0)
+{
+    result += num % 2 * mult;
+    num = num / 2;
+    mult *= 10;
+}
+
+Console.WriteLine(result);
+
+
+
+// Вариант 2
+// Console.WriteLine("Введите целое число: ");
+// int decimalNumber = Convert.ToInt32(Console.ReadLine());
+// int temp = decimalNumber;
+// int count = 0;
+
+
+// int[] binaryNumber = new int[20];
+
+// while (temp != 0)
+// {
+//     binaryNumber[19 - count] = temp % 2;
+//     temp /= 2;
+//     count++;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     int i = 0;
+//     while (array[i] == 0)
+//     {
+//         i++;
+//     }
+//     while (i < array.Length)
+//     {
+//         Console.Write($"{array[i]}");
+//         i++;
+//     }
+
+//     Console.WriteLine();
+// }
+
+// PrintArray(binaryNumber);
+
+
+// Варриант 3
+int number = 13;
+
+void DecToBin(int num)
+{
+    if (num == 0) return;
+    DecToBin(num / 2);
+    Console.Write(num % 2);
+}
+
+DecToBin(number);
