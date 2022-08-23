a20ba41d63f03722f749f380ed8d26388f4fc307
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-23 11:00:21 +0300

    Задача 44

---
+++ Zadacha 44/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+
+Console.WriteLine("Введите положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// fibonacci(n) = n-1 + n-2
+// fibonacci(0) = 0 проверка if
+// fibonacci(1) = 1 проверка if
+if (n == 0) Console.WriteLine("0");
+if (n == 1) Console.WriteLine("1");
+Console.Write("0 1 ");
+int fibonacci = 2;
+int number1 = 0;
+int number2 = 1;
+for (int i = 2; i < n; i++)
+{
+
+    fibonacci = number1 + number2;
+    number1 = number2;
+    number2 = fibonacci;
+    Console.Write($"{fibonacci} ");
+}
+
+
+// Вариант 2
+
+// Console.Write("Введите целое положительное число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// void ShowFibonacci(int num)
+// {
+//     int num1 = 0;
+//     int num2 = 1;
+
+//     Console.Write(num1+" "+num2+" ");
+
+//     for (int i = 2; i < num; i++)
+//     {
+//         int temp = num1 + num2;
+//         num1 = num2;
+//         num2 = temp;
+//         Console.Write(temp+" ");    
+//     }
+//     Console.WriteLine();
+// }
+
+// ShowFibonacci(number);
