620c8fd15ea67987797d541c8e4ca5de496c34d8
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-27 11:31:07 +0300

    Задача 45 за коментированно

--- Zadacha 45/Program.cs
+++ Zadacha 45/Program.cs
@@ -2,24 +2,24 @@
 // копию заданного массива с помощью поэлементного
 // копирования.
 
-int[] ReturnArray(int[] array)
-{
-    int[] arr = new int[array.Length];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = array[i];
-    }
-    return arr;
-}
+// int[] ReturnArray(int[] array)
+// {
+//     int[] arr = new int[array.Length];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = array[i];
+//     }
+//     return arr;
+// }
 
-int[] arr1 = CreateArray(10, 1, 20);
+// int[] arr1 = CreateArray(10, 1, 20);
 
-PrintArray(arr1);
-int[] arr2 = ReturnArray(arr1);
-PrintArray(arr2);
+// PrintArray(arr1);
+// int[] arr2 = ReturnArray(arr1);
+// PrintArray(arr2);
 
-Console.WriteLine("Изменим втрое значение в массиве, чтобы определить скопированный массив");
+// Console.WriteLine("Изменим втрое значение в массиве, чтобы определить скопированный массив");
 
-arr1[1] = 30;
-PrintArray(arr1);
-PrintArray(arr2);
+// arr1[1] = 30;
+// PrintArray(arr1);
+// PrintArray(arr2);
