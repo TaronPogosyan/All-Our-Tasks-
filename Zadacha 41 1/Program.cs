8b4f454a80b25dcfa18a3cb4721eeac0d0b4adc2
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-27 18:02:05 +0300

    Zadacha 41 1

---
+++ Zadacha 41 1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+Console.Write($"Количество чисел, M ввел пользователь: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] masNum = new int[m];
+
+
+int CreateNum(int[] masNum)
+{
+    int count = 0;
+    for (int i = 0; i < masNum.Length; i++)
+    {
+        if (masNum[i] > 0) count += 1;
+    }
+    return count;
+}
+
+void PrintNum(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Число {i + 1}: ");
+        masNum[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+PrintNum(m);
+
+Console.WriteLine($"Пользователь ввел больше 0: {CreateNum(masNum)} чисел.");
